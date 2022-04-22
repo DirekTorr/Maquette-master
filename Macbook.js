67d7a6f4cd3979d5deb6c1e037130604ae40fee2
DirekTorr <pauldelpech@ymail.com>
2022-04-23 00:02:14 +0200

    Push derniers fichiers

--- Macbook.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import ReactPlayer from 'react-player/youtube'
-
-// Only loads the YouTube player
-
-        <ReactPlayer url='https://www.youtube.com/watch?v=ZitQd2Nd-Ds' />
